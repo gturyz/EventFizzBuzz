c15fc51cb2f5a2a01105180ccdf8e8e5c0fab142
Léo Bonhême--Morsent <gturyz@gmail.com>
2022-04-29 12:49:12 +0200

    OK : Deuxième condition multiple de 5 & 3

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ emitter.on('FizzBuzz', function(arg) {
     if(isNaN(arg)) {
         console.log("Ce n'est pas un nombre")
     } else {
-        if (arg % 5 == 0) {
+        if (arg % 5 == 0 && arg % 3 == 0) {
+            console.log("FizzBuzz")
+        } else if (arg % 5 == 0) {
             console.log("Buzz")
         }
     }
